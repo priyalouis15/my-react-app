009b498b57501b607613bc81e8b8b270037380fe
priyalouis15 <priyankalouis30@gmail.com>
2026-04-23 10:10:16 +0530

    Updated frontend with backend integration

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import BASE_URL from "../api";
 import Category from "./Category";
 import Carousel from "./carousel";
 import ProductCard from "./ProductCard";
